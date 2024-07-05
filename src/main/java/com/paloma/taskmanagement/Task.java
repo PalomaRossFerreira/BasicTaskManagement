a9fe06751080cd748a225d95ec956a2655c4f676
PalomaRossFerreira <rossferreira.paloma@gmail.com>
2024-07-05 11:44:41 -0300

    task-1.2

---
+++ src/main/java/com/paloma/taskmanagement/Task.java
@@ -0,0 +1,72 @@
+package com.paloma.taskmanagement;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Task implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Integer id;
+	private String title;
+	private int dueDate;
+	private String status;
+	
+	public Task() {
+		
+	}
+
+	public Task(Integer id, String title, int dueDate, String status) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.dueDate = dueDate;
+		this.status = status;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public int getDueDate() {
+		return dueDate;
+	}
+
+	public void setDueDate(int dueDate) {
+		this.dueDate = dueDate;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	@Override
+	public String toString() {
+		return "Task [id=" + id + ", title=" + title + ", dueDate=" + dueDate + ", status=" + status + "]";
+	}
+
+	
+	
+}
--- src/main/java/com/paloma/taskmanagement/TaskmanagementApplication.java
+++ src/main/java/com/paloma/taskmanagement/TaskmanagementApplication.java
@@ -1,13 +1,27 @@
 package com.paloma.taskmanagement;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 
-@SpringBootApplication
 public class TaskmanagementApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TaskmanagementApplication.class, args);
+		//Task t1 = new Task(null, "Criar Entity Manager", 0607, "Doing");.//
+		
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
+		EntityManager em = emf.createEntityManager();
+		
+		Task t = em.find(Task.class, 1);
+		
+		System.out.println(t);
+		
+		//em.getTransaction().begin();//
+		//em.persist(t1);//
+		//em.getTransaction().commit();//
+		System.out.println("Pronto!");
+		em.close();
+		emf.close();
 	}
 
 }
